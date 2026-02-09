9d36c98a00b9027223b2c97555578cdb10d90faf
ritik <ritik2501997@gmail.com>
2026-02-09 22:30:46 +0530

    add new problem on pattern

---
+++ FloydTrangle.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class FloydTrangle{
+    public static void main(String[] args) {
+                                                                      /*
+                                                                      1
+                                                                      2 3
+                                                                      4 5 6
+                                                                      7 8 9 10
+                                                                      11 12 13 14 
+                                                                  */
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the size of row");
+    int n=sc.nextInt();
+    int x=1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            System.out.print(x+" ");
+            x++;
+
+        }
+        System.out.println();
+
+    }                                                                  
+        
+    }
+    
+}
